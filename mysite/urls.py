ae595a29858353ac001bbadf5feeb6d91d6e9c8d
steve <sjpearce@gmail.com>
2021-02-18 11:21:38 +0000

    pages

--- mysite/urls.py
+++ mysite/urls.py
@@ -6,6 +6,7 @@ from django.contrib.auth import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls')),
+    path('pages/', include('pages.urls')),
     path('accounts/login/', views.LoginView.as_view(), name='login'),
     path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
     path('', include('blog.urls')),
